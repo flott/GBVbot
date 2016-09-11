9f24f8e812e64705cb3a3003747592fc8d2c4e1c
Fred <lott.fred@gmail.com>
2016-09-11 21:59:40 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# GBV title bot
+
+Titles from the wonderful Guided By Voices database at http://www.gbvdb.com/home.asp
+Lists scraped 2016-09-10
+Includes all songs released/unreleased, played live, dialog tracks, improvised, etc.
---
+++ gbv_markovify.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+# Markovify
+# https://github.com/jsvine/markovify
+# https://github.com/jsvine/markovify/blob/master/markovify/text.py
+import markovify
+import random
+import tweepy
+from time import sleep
+from secrets_json import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET
+
+TWEET_INTERVAL = 60 * 15 # time in seconds
+
+with open("gbvtitles.txt") as f:
+    text = f.read()
+
+text_model = markovify.NewlineText(text)
+
+def make_title():
+    if random.random() <= 0.05:
+        # add a parentheses suffix to the title
+        first_len = random.randrange(20,120)
+        second_len = random.randrange(16, 140 - first_len)
+        
+        first_part = text_model.make_short_sentence(first_len,tries=100)
+        second_part = text_model.make_short_sentence(second_len,tries=100)
+        title = first_part + " (" + second_part + ")"
+        return title
+    else:
+        return text_model.make_short_sentence(20 + random.randrange(20,120),tries=100)
+
+def send_msg(api, msg):
+    try:
+        status = api.update_status(status=msg)
+    except tweepy.error.TweepError as e:
+        print(repr(e))
+
+def main():
+    # Twitter API setup
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+    api = tweepy.API(auth)
+
+    while True:
+        title = make_title()
+        print("toot is: ",title)
+        send_msg(api, title)
+        sleep(TWEET_INTERVAL)
+
+if __name__ == '__main__':
+    main()
